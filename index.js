d5595df388bfa883ba6d71b2cc9d87d2eedf30be
PrateekTechmatys <prateeky562@gmail.com>
2024-02-26 18:50:15 +0530

    second commit of day 3

---
+++ index.js
@@ -0,0 +1,94 @@
+const question1=[
+    {
+        Question:"Who is the God of Cricket",
+        Option:[
+            {
+                text:"Sachin tendulkar",
+                correct:true
+            },
+            {
+                text:"Rohit Sharma",
+                correct:false
+            },
+            {
+                text:"MS Dhoni",
+                correct:false
+            },
+            {
+                text:"Virat Kohli",
+                correct:false
+            }
+        ]
+    },
+    {
+        Question:"Who is the God of Cricket",
+        Option:[
+            {
+                text:"Sachin tendulkar",
+                correct:true
+            },
+            {
+                text:"Rohit Sharma",
+                correct:false
+            },
+            {
+                text:"MS Dhoni",
+                correct:false
+            },
+            {
+                text:"Virat Kohli",
+                correct:false
+            }
+        ]
+    },
+    {
+        Question:"Who is the God of Cricket",
+        Option:[
+            {
+                text:"Sachin tendulkar",
+                correct:true
+            },
+            {
+                text:"Rohit Sharma",
+                correct:false
+            },
+            {
+                text:"MS Dhoni",
+                correct:false
+            },
+            {
+                text:"Virat Kohli",
+                correct:false
+            }
+        ]
+    },
+    {
+        Question:"Who is the God of Cricket",
+        Option:[
+            {
+                text:"Sachin tendulkar",
+                correct:true
+            },
+            {
+                text:"Rohit Sharma",
+                correct:false
+            },
+            {
+                text:"MS Dhoni",
+                correct:false
+            },
+            {
+                text:"Virat Kohli",
+                correct:false
+            }
+        ]
+    }
+]
+
+const question = document.getElementById('question');
+const options = document.getElementById('options');
+const next_btn = document.getElementById('next_btn');
+
+let current=0;
+let score=0;
+
