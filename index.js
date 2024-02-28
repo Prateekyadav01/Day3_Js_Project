e3cc984d2c35a01f4f2b7fe7daad69d717b355dc
PrateekTechmatys <prateeky562@gmail.com>
2024-02-28 21:10:31 +0530

    adding something in it

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const question1=[
     {
-        Question:"Who is the God of Cricket",
+        Question:"Who is the God of Cricket?",
         Option:[
             {
                 text:"Sachin tendulkar",
@@ -92,3 +92,30 @@ const next_btn = document.getElementById('next_btn');
 let current=0;
 let score=0;
 
+function add(){
+    console.log(current)
+    let firstShow = question1[current];
+    // question.innerText=null;
+    question.innerText= `${current+1}  ${firstShow.Question}`;
+    options.innerHTML = '';
+    firstShow.Option.forEach(option=>{
+        let optionButton = document.createElement('button');
+        
+        optionButton.textContent=option.text;
+        optionButton.addEventListener("click", () => handleCheck(option.correct));
+        options.appendChild(optionButton)
+    })
+    
+}
+function handleCheck(value){
+    if(value){
+        score++;
+        options.style.backgroundColor="green"
+    }
+   current++;
+    if(current<question1.length){
+            add();
+    }
+}
+next_btn.addEventListener('click', add);
+add();
